3d0f53a482041185a9a5b355ed24c0c91b93e1de
Eduardo Julião Junior <eduardo.juliaojr@hotmail.com>
2020-11-26 13:57:02 +0000

    some fixes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { showCLIName } from './lib/display';
 import configureHelper from './jwt-cli.args';
 import { runEnvCheck } from './lib/environment.validator';
@@ -8,14 +9,21 @@ showCLIName();
 var argv = configureHelper();
 
 const pathToConfig = argv.pathToConfig as string;
-const command = argv._[0];
+const command = argv._.length === 0 ? '' : argv._[0];
 
 if (!runEnvCheck()) process.exit();
 
 // for now, only creates a valid token:
 switch(command){
     case 'create-new':
-        createToken(pathToConfig)
+        const token = createToken(pathToConfig);
+        console.log('\n');
+        console.log('************Token************');
+        console.log('\n');
+        console.log(token);
+        console.log('\n');
+        console.log('************Token************');
+        console.log('\n');
         break;
     default:
         console.log('invalid command.');
--- src/jwt-cli.args.ts
+++ src/jwt-cli.args.ts
@@ -4,11 +4,11 @@ import { Arguments } from "yargs";
 export default function configureHelper(): {
   [key in keyof Arguments<{}>]: Arguments<{}>[key];
 } {
-  return yargs.options("pathToConfig", {
+  return yargs
+  .command('create-new', 'creates a new token.')
+  .option("pathToConfig", {
     demandOption: true,
     type: "string",
     description: "Full path to the json config file",
-  })
-  .command('create-new', 'creates a new token.')
-  .argv;
+  }).argv;
 }
